df0d024c18fe9cfa89d80093985a72751bfa86b7
shpaker <shpaker@gmail.com>
2021-07-26 21:05:17 +0300

    init

--- plugins/handlers.py
+++ plugins/handlers.py
@@ -3,15 +3,14 @@ import os
 
 from pyrogram import Client, filters
 
+logging.basicConfig(level=logging.INFO, datefmt="%Y/%m/%d %H:%M:%S", format="%(levelname)s: %(message)s")
 
-logging.basicConfig(level=logging.INFO, datefmt='%Y/%m/%d %H:%M:%S', format='%(levelname)s: %(message)s')
-
-CHANNELS_LIST = os.getenv('CHANNELS').split(',')
-CHAT_IDS = [int(chat_id) for chat_id in os.getenv('CHAT_IDS').split(',')]
+CHANNELS_LIST = os.getenv("CHANNELS").split(",")
+CHAT_IDS = [int(chat_id) for chat_id in os.getenv("CHAT_IDS").split(",")]
 
 
 @Client.on_message(filters.channel)
-async def channel_handler(client, message):
+async def channel_handler(_, message):
     if message.chat.username in CHANNELS_LIST and not message.edit_date:
         logging.info(message.chat)
         for chat_id in CHAT_IDS:
