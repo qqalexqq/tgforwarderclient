9f0a9b9f2f1c388398bd6c4e6547a6ade374b18d
qqalexqq <qqalexqq@yandex.ru>
2021-08-01 02:45:58 +0300

    Add Dockerfile, move code into it, add SESSION_KEY

--- Pyrogram/app.py
+++ Pyrogram/app.py
@@ -1,17 +1,27 @@
 import logging
 import os
 
-from pyrogram import Client, filters
+from pyrogram import Client, filters, handlers
 
 logging.basicConfig(level=logging.INFO, datefmt="%Y/%m/%d %H:%M:%S", format="%(levelname)s: %(message)s")
 
 CHANNELS_LIST = os.getenv("CHANNELS").split(",")
 CHAT_IDS = [int(chat_id) for chat_id in os.getenv("CHAT_IDS").split(",")]
+SESSION_STRING = os.getenv("SESSION_KEY", "bot_session")
 
 
-@Client.on_message(filters.channel)
 async def channel_handler(_, message):
     if message.chat.username in CHANNELS_LIST and not message.edit_date:
         logging.info(message.chat)
         for chat_id in CHAT_IDS:
             await message.forward(chat_id)
+
+
+def main():
+    app = Client(SESSION_STRING, os.getenv('API_ID'), os.getenv('API_HASH'))
+
+    app.add_handler(handlers.MessageHandler(channel_handler, filters.channel))
+    app.run()
+
+
+main()
