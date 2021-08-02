20f9a7645462e844eead92384b7e7d1ad2183ac6
shpaker <shpaker@gmail.com>
2021-08-02 21:52:35 +0300

    .

--- tg_forwarder_client.py
+++ tg_forwarder_client.py
@@ -1,25 +1,44 @@
 import logging
-import os
+from dataclasses import field
+from functools import lru_cache
+from typing import List
 
+from dataclasses_settings import dataclass_settings
+from dataclasses_settings.params import field_params
 from pyrogram import Client, filters, handlers
 
-logging.basicConfig(level=logging.INFO, datefmt="%Y/%m/%d %H:%M:%S", format="%(levelname)s: %(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    datefmt="%Y/%m/%d %H:%M:%S",
+    format="%(levelname)s: %(message)s",
+)
 
-CHANNELS_LIST = os.getenv("CHANNELS").split(",")
-CHAT_IDS = [int(chat_id) for chat_id in os.getenv("CHAT_IDS").split(",")]
-SESSION_STRING = os.getenv("SESSION_KEY", "bot_session")
+
+@dataclass_settings(prefix="bot_")
+class Settings:
+    api_id: int
+    api_hash: str
+    channels: List[str]
+    chats: List[int]
+    session: str = field(metadata=field_params(alias="session_key"))
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
 
 
 async def channel_handler(_, message):
-    if message.chat.username in CHANNELS_LIST and not message.edit_date:
+    settings = get_settings()
+    if message.chat.username in settings.channels and not message.edit_date:
         logging.info(message.chat)
-        for chat_id in CHAT_IDS:
+        for chat_id in settings.chats:
             await message.forward(chat_id)
 
 
 def main():
-    app = Client(SESSION_STRING, os.getenv('API_ID'), os.getenv('API_HASH'))
-
+    settings = get_settings()
+    app = Client(settings.session, settings.api_id, settings.api_hash)
     app.add_handler(handlers.MessageHandler(channel_handler, filters.channel))
     app.run()
 
