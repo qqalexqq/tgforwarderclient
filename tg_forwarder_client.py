93692bcc2e0d2533363bd5db50456d0567457e79
qqalexqq <qqalexqq@users.noreply.github.com>
2021-08-06 21:28:54 +0300

    Add proper media_group forward. (#7)
    Co-authored-by: Aleksandr Shpak <shpaker@users.noreply.github.com>

--- tg_forwarder_client.py
+++ tg_forwarder_client.py
@@ -1,4 +1,5 @@
 import logging
+import asyncio
 from dataclasses import field
 from functools import lru_cache
 from typing import List
@@ -6,12 +7,18 @@ from typing import List
 from dataclasses_settings import dataclass_settings
 from dataclasses_settings.params import field_params
 from pyrogram import Client, filters, handlers
+from pyrogram.types import Message
+
 
 logging.basicConfig(
     level=logging.INFO,
     datefmt="%Y/%m/%d %H:%M:%S",
     format="%(levelname)s: %(message)s",
 )
+
+
+loop = asyncio.get_event_loop()
+queues = {}
 
 
 @dataclass_settings(prefix="bot_")
@@ -28,12 +35,37 @@ def get_settings() -> Settings:
     return Settings()
 
 
-async def channel_handler(_, message):
+def send_media_group(
+    client: Client,
+    media_group_id: int,
+) -> None:
     settings = get_settings()
-    if message.chat.username in settings.channels and not message.edit_date:
-        logging.info(message.chat)
-        for chat_id in settings.chats:
-            await message.forward(chat_id)
+
+    for chat_id in settings.chats:
+        loop.create_task(client.forward_messages(chat_id, *queues[media_group_id]))
+
+
+async def channel_handler(
+    client: Client,
+    msg: Message,
+) -> None:
+    settings = get_settings()
+    if msg.chat.username not in settings.channels or msg.edit_date:
+        return
+
+    logging.info(msg.chat)
+
+    if hasattr(msg, 'media_group_id') and msg.media_group_id:
+        if msg.media_group_id not in queues:
+            queues[msg.media_group_id] = (msg.chat.id, [])
+            loop.call_later(10, send_media_group, client, msg.media_group_id)
+
+        queues[msg.media_group_id][1].append(msg.message_id)
+
+        return
+
+    for chat_id in settings.chats:
+        await msg.forward(chat_id)
 
 
 def main():
