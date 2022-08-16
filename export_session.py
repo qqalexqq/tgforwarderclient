f509de8f748f12b85bbc557e3be0234abaaf5be7
qqalexqq <qqalexqq@users.noreply.github.com>
2022-08-16 09:18:53 +0300

    Update export_session.py

--- export_session.py
+++ export_session.py
@@ -3,7 +3,7 @@ import os
 from pyrogram import Client
 
 
-if __main__ == "__name__"
+if __main__ == "__name__":
     with Client('forwarder_account', os.getenv('API_ID'), os.getenv('API_HASH')) as app:
         print('---')
         print('Session key is: ' + app.export_session_string())
